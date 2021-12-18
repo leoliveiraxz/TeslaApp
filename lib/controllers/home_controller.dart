8e0e12ba210d663dfd5a8f56657f0750586b9a79
leoliveiraxz <luis.robert.5@live.com>
2021-12-18 17:03:50 -0300

    Adição de novos botões e da barra de menu inferior

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -3,13 +3,24 @@ import 'package:flutter/cupertino.dart';
 class HomeController extends ChangeNotifier{
   bool isRightDoorLocked = true;
   bool isLeftDoorLocked = true;
+  bool isBonnetLocked = true;
+  bool isTrunkLocked = true;
+
 
   void updateRightDoorLock() {
     isRightDoorLocked = !isRightDoorLocked;
     notifyListeners();
   }
   void updateLeftDoorLock() {
-    isRightDoorLocked = !isRightDoorLocked;
+    isLeftDoorLocked = !isLeftDoorLocked;
+    notifyListeners();
+  }
+  void updateBonnetDoorLock() {
+    isBonnetLocked = !isBonnetLocked;
+    notifyListeners();
+  }
+  void updateTrunkDoorLock() {
+    isTrunkLocked = !isTrunkLocked;
     notifyListeners();
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: unnecessary_string_escapes
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:tesla/constraints.dart';
 import 'package:tesla/controllers/home_controller.dart';
 
 import 'componentes/door_lock.dart';
+import 'componentes/tesla_botton_navigationBar.dart';
 
 class HomeScreen extends StatelessWidget {
   final homeController = HomeController();
@@ -16,31 +16,46 @@ class HomeScreen extends StatelessWidget {
     return AnimatedBuilder(
         animation: homeController,
         builder: (context, snapshot) {
-          return Scaffold(body:
-              SafeArea(child: LayoutBuilder(builder: (context, constraits) {
-            return Stack(
-              alignment: Alignment.center,
-              children: [
-                Padding(
-                    padding: EdgeInsets.symmetric(
-                        vertical: constraits.maxHeight * 0.1),
-                    child: SvgPicture.asset("assets/icons/Car.svg",
-                        width:
-                            double.infinity //MediaQuery.of(context).size.width
-                        )),
-                Positioned(
-                    right: constraits.maxWidth * 0.05,
-                    child: DoorLock(
-                        isLock: homeController.isRightDoorLocked,
-                        press: homeController.updateRightDoorLock)),
-                Positioned(
-                    left: constraits.maxWidth * 0.05,
-                    child: DoorLock(
-                        isLock: homeController.isRightDoorLocked,
-                        press: homeController.updateRightDoorLock))
-              ],
-            );
-          })));
+          return Scaffold(
+              bottomNavigationBar: TeslaBottomNavigationBar(
+                onTap: (value) {},
+                selectedTab: 0,
+              ),
+              body:
+                  SafeArea(child: LayoutBuilder(builder: (context, constraits) {
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: constraits.maxHeight * 0.1),
+                        child: SvgPicture.asset("assets/icons/Car.svg",
+                            width: double
+                                .infinity //MediaQuery.of(context).size.width
+                            )),
+                    Positioned(
+                        right: constraits.maxWidth * 0.05,
+                        child: DoorLock(
+                            isLock: homeController.isRightDoorLocked,
+                            press: homeController.updateRightDoorLock)),
+                    Positioned(
+                        left: constraits.maxWidth * 0.05,
+                        child: DoorLock(
+                            isLock: homeController.isLeftDoorLocked,
+                            press: homeController.updateLeftDoorLock)),
+                    Positioned(
+                        top: constraits.maxHeight * 0.13,
+                        child: DoorLock(
+                            isLock: homeController.isBonnetLocked,
+                            press: homeController.updateBonnetDoorLock)),
+                    Positioned(
+                        bottom: constraits.maxHeight * 0.17,
+                        child: DoorLock(
+                            isLock: homeController.isTrunkLocked,
+                            press: homeController.updateTrunkDoorLock))
+                  ],
+                );
+              })));
         });
   }
 }
